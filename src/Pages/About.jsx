910f3a753454ce569dae0c388452d213a54d6914
DavidOsezua <102161226+DavidOsezua@users.noreply.github.com>
2024-02-23 09:43:35 +0100

    updated staff page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
+import styles from "./AboutUs.module.css";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="pt-[8rem]">
+      <div className={`${styles.container}`}>About us</div>
+    </section>
+  );
 };
 
 export default About;
--- src/Pages/Staffs.jsx
+++ src/Pages/Staffs.jsx
@@ -19,7 +19,7 @@ const Staffs = () => {
   return (
     <section className="pt-[8rem] bg-[#dddada]">
       <div className={`${styles.container}`}>
-        <h2 className={`${styles.title} text-center`}>
+        <h2 className={`${styles.title} ${styles.center} `}>
           Meet our <span className="text-[#754FE2]"> Staffs</span>
         </h2>
 
@@ -54,7 +54,7 @@ const Staffs = () => {
             <img src={DrEtsename} className={`${styles.image}`} />
             <div className={`${styles.rank}`}>
               <h2 className={`${styles.title}`}>Dr. E.L Etsename</h2>
-              <p>Senior graphics lecturer</p>
+              <p>Graphics lecturer</p>
             </div>
           </div>
         </div>
@@ -64,14 +64,14 @@ const Staffs = () => {
           <div className={`${styles.imageWrapper}`}>
             <img src={DrOdeyemi} className={`${styles.image}`} />
             <div className={`${styles.rank}`}>
-              <h2>Prof. O.F Kayode</h2>
-              <p>Senior graphics lecturer</p>
+              <h2 className={`${styles.title}`}>Dr. Odeyemi</h2>
+              <p>Chief technologist</p>
             </div>
           </div>
           <div className={`${styles.imageWrapper}`}>
             <img src={MrUthman} className={`${styles.image}`} />
             <div className={`${styles.rank}`}>
-              <h2>Prof. O.F Kayode</h2>
+              <h2 className={`${styles.title}`}>Mr.Uthman</h2>
               <p>Senior graphics lecturer</p>
             </div>
           </div>
@@ -79,8 +79,8 @@ const Staffs = () => {
           <div className={`${styles.imageWrapper}`}>
             <img src={DrDare} className={`${styles.image}`} />
             <div className={`${styles.rank}`}>
-              <h2>Prof. O.F Kayode</h2>
-              <p>Senior graphics lecturer</p>
+              <h2 className={`${styles.title}`}>Dr. Dare</h2>
+              <p>Graphics technologist</p>
             </div>
           </div>
         </div>
@@ -90,37 +90,39 @@ const Staffs = () => {
           <div className={`${styles.imageWrapper}`}>
             <img src={ProfKashim} className={`${styles.image}`} />
             <div className={`${styles.rank}`}>
-              <h2>Prof. O.F Kayode</h2>
-              <p>Senior graphics lecturer</p>
+              <h2 className={`${styles.title}`}>Prof. I.B Kashim</h2>
+              <p>Senior ceramics lecturer</p>
             </div>
           </div>
 
           <div className={`${styles.imageWrapper}`}>
             <img src={DrErhuanga} className={`${styles.image}`} />
             <div className={`${styles.rank}`}>
-              <h2>Prof. O.F Kayode</h2>
-              <p>Senior graphics lecturer</p>
+              <h2 className={`${styles.title}`}>Dr E.A Erhuanga</h2>
+              <p>Senior ceramics lecturer</p>
             </div>
           </div>
           <div className={`${styles.imageWrapper}`}>
             <img src={DrAdelabu} className={`${styles.image}`} />
             <div className={`${styles.rank}`}>
-              <h2>Prof. O.F Kayode</h2>
-              <p>Senior graphics lecturer</p>
+              <h2 className={`${styles.title}`}>Dr. O.S Adelabu</h2>
+              <p>Senior ceramics lecturer</p>
             </div>
           </div>
           <div className={`${styles.imageWrapper}`}>
             <img src={DrAjala} className={`${styles.image}`} />
             <div className={`${styles.rank}`}>
-              <h2>Prof. O.F Kayode</h2>
-              <p>Senior graphics lecturer</p>
+              <h2 className={`${styles.title}`}>Dr. Ajala</h2>
+              <h2 className={`${styles.title}`}>Dr.Ajala</h2>
+              <p>Senior ceramics lecturer</p>
             </div>
           </div>
           <div className={`${styles.imageWrapper}`}>
             <img src={DrOlogunwa} className={`${styles.image}`} />
             <div className={`${styles.rank}`}>
-              <h2>Prof. O.F Kayode</h2>
-              <p>Senior graphics lecturer</p>
+              <h2 className={`${styles.title}`}>Dr.Ologunwa</h2>
+
+              <p>Senior ceramics lecturer</p>
             </div>
           </div>
         </div>
